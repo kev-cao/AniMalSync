8f2f756d27af8ca237cf177b0ac34362461f8e8d
Kevin Cao <kcao1998@gmail.com>
2022-06-30 18:59:30 -0400

    build: certbot expand existing certificate

--- web/.platform/hooks/postdeploy/00_get_certificate.sh
+++ web/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d ${HOST_IP} -d www.${HOST_IP} --nginx --agree-tos --email ${EMAIL}
+sudo certbot -n -d ${HOST_IP} -d www.${HOST_IP} --nginx --agree-tos --email ${EMAIL} --expand
